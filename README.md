beb15f3a5c26a2801c2df9650ceb816d220e5f41
junqiangchen <1207173174@qq.com>
2018-08-28 16:26:00 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# VNet3D
+# ImageSegmentation With Vnet3D
+> This is an example of the prostate in transversal T2-weighted MR images Segment from MICCAI Grand Challenge:Prostate MR Image Segmentation 2012
+![](promise12_header.png)
+
+## How to Use
+
+**1、download trained data,download dataset：https://promise12.grand-challenge.org/download/**
+
+**2、the file of promise12Vnet3dImage.csv,is like this format:
+  D:\Data\PROMISE2012\Vnet3d_data\Vnet3d_patch_train\image/0_10
+  D:\Data\PROMISE2012\Vnet3d_data\Vnet3d_patch_train\image/0_11
+  D:\Data\PROMISE2012\Vnet3d_data\Vnet3d_patch_train\image/0_12
+  ......
+if you trained data path is not D:\Data\PROMISE2012\,you should change the csv file path just like this:using C:\Data\ replace D:\Data\PROMISE2012\.**
+
+**3、when data is prepared,just run the vnet3d_train_predict.py**
+
+**4、training the model on the GTX1080,it take 40 hours,and i also attach the trained model in the project,you also just use the vnet3d_train_predict.py file to predict,and get the segmentation result.**
+
+**5、download trained model:https://pan.baidu.com/s/19E9q6HIUeRB8jpuNhvE2Zg, passworld:obwu**
+
+## Result
+
+the trained loss result
+![](loss.PNG)
+the Vnet3D model
+![](vnet.PNG)
+the trained process:0 epoch——GTMask and PredictMask
+![](gt_0_epoch.PNG)
+![](predict_0_epoch.PNG)
+1000 epoch——GTMask and PredictMask
+![](gt_1000_epoch.PNG)
+![](predict_1000_epoch.PNG)
+10000 epoch——GTMask and PredictMask
+![](gt_10000_epoch.PNG)
+![](predict_10000_epoch.PNG)
+the predict result
+![](mask_15_epoch.PNG)
+![](src_15_epoch.PNG)
+## Contact
+* https://github.com/junqiangchen
+* email: 1207173174@qq.com
+* WeChat Public number: 最新医学影像技术
---
+++ promise2012/vnet3d_train_predict.py
@@ -0,0 +1,48 @@
+from Vnet.model_vnet3d import Vnet3dModule
+import numpy as np
+import pandas as pd
+import cv2
+
+
+def train():
+    '''
+    Preprocessing for dataset
+    '''
+    # Read  data set (Train data from CSV file)
+    csvmaskdata = pd.read_csv('promise12Vnet3dMask.csv')
+    csvimagedata = pd.read_csv('promise12Vnet3dImage.csv')
+    maskdata = csvmaskdata.iloc[:, :].values
+    imagedata = csvimagedata.iloc[:, :].values
+    # shuffle imagedata and maskdata together
+    perm = np.arange(len(csvimagedata))
+    np.random.shuffle(perm)
+    imagedata = imagedata[perm]
+    maskdata = maskdata[perm]
+
+    Vnet3d = Vnet3dModule(128, 128, 64, channels=1, costname="dice coefficient")
+    Vnet3d.train(imagedata, maskdata, "model\\Vnet3dModule.pd", "log\\", 0.001, 0.7, 100000, 1)
+
+
+def predict0():
+    Vnet3d = Vnet3dModule(256, 256, 64, inference=True, model_path="model\\Vnet3dModule.pd")
+    for filenumber in range(30):
+        batch_xs = np.zeros(shape=(64, 256, 256))
+        for index in range(64):
+            imgs = cv2.imread(
+                "D:\Data\PROMISE2012\Vnet3d_data\\test\image\\" + str(filenumber) + "\\" + str(index) + ".bmp", 0)
+            batch_xs[index, :, :] = imgs[128:384, 128:384]
+
+        predictvalue = Vnet3d.prediction(batch_xs)
+
+        for index in range(64):
+            result = np.zeros(shape=(512, 512), dtype=np.uint8)
+            result[128:384, 128:384] = predictvalue[index]
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+            result = cv2.morphologyEx(result, cv2.MORPH_CLOSE, kernel)
+            cv2.imwrite(
+                "D:\Data\PROMISE2012\Vnet3d_data\\test\image\\" + str(filenumber) + "\\" + str(index) + "mask.bmp",
+                result)
+
+
+train()
+#predict0()
